3256a38998cff66a68d66ab3dfa01d07e2263b6b
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-10-25 18:19:08 +0000

    Improved formatting slightly

--- wiki.py
+++ wiki.py
@@ -9,7 +9,8 @@ try:
     json_data = json.load(urllib2.urlopen("http://en.wikipedia.org/w/api.php?action=query&titles=" + title + "&prop=extracts&exintro=&explaintext=&rvprop=content&format=json"));
     pages = json_data["query"]["pages"]
     page_key = pages.keys()[0];
+    print "====="
     print pages[page_key]["extract"]
+    print "====="
 except urllib2.URLError:
-    print "===\n"
     print "There was an error when opening the URL :( \nYou may not be connected to the interwebs!"
