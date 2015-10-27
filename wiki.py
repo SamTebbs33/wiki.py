c5807e4e54379ddffc92f1a3ee5446ee191f7cd3
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-10-27 09:53:39 +0000

    Added more error checking

--- wiki.py
+++ wiki.py
@@ -7,10 +7,14 @@ title = " ".join(sys.argv[1:])
 title = urllib2.quote(title)
 try:
     json_data = json.load(urllib2.urlopen("http://en.wikipedia.org/w/api.php?action=query&titles=" + title + "&prop=extracts&exintro=&explaintext=&rvprop=content&format=json"));
-    pages = json_data["query"]["pages"]
-    page_key = pages.keys()[0];
-    print "====="
-    print pages[page_key]["extract"]
-    print "====="
+    try:
+        pages = json_data["query"]["pages"]
+        page_key = pages.keys()[0];
+        text =  pages[page_key]["extract"]
+        print "====="
+        print text
+        print "====="
+    except KeyError:
+        print "That page doesn't seem to exist, try changing your query!"
 except urllib2.URLError:
     print "There was an error when opening the URL :( \nYou may not be connected to the interwebs!"
